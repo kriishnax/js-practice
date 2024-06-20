5b25d9b1b6a8092badfe3058a65cae95929b560f
Krishna <krishnasusari@gmail.com>
2024-06-20 09:42:21 +0530

    Update variable.js

--- Basic/variable.js
+++ Basic/variable.js
@@ -1,2 +1,11 @@
 const accountId = 14569;
+let accountMail = 'krishnasusari@gmail.com';
+var accountPassword = 12356;
+accountCity = 'Banglore';
+
+
+
 console.log(accountId);
+console.log(accountMail);
+console.log(accountPassword);
+console.log(accountCity);
