0ad87018c1298e1a1259b21ca388a0d67372e1d2
Krishna <krishnasusari@gmail.com>
2024-06-20 10:07:30 +0530

    dateypes

---
+++ Basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 14569;
+let accountMail = 'krishnasusari@gmail.com';
+var accountPassword = 12356;
+accountCity = 'Banglore';
+
+
+accountCity = 'Bhub';
+//here const cannt be change let, var can change its value if delared later.
+
+console.log(accountId);
+console.log(accountMail);
+console.log(accountPassword);
+console.log(accountCity);
---
+++ Basic/02_datatype.js
@@ -0,0 +1,15 @@
+let name = " krix";
+let age = 18 ;
+let city = 'bhub';
+let cgpa = 9.1;
+
+
+console.log( typeof name);
+console.log( typeof age);
+console.log( typeof city);
+console.log( typeof cgpa);
+
+/*string
+number
+string
+number  */
